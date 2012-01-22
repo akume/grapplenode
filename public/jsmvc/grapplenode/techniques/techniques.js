0d93b843d1022f5366b32c6564f45639bf74cffc
Akume <akume325@gmail.com>
2012-01-21 19:31:31 -0800

    modified the techniques section to be more general, locker will have personal items

--- public/jsmvc/grapplenode/techniques/techniques.js
+++ public/jsmvc/grapplenode/techniques/techniques.js
@@ -13,29 +13,123 @@ $.Controller('Grapplenode.Techniques',
 /** @Static */
 {
 	defaults :
-  {
-    params: new Mxui.Data({ filter: "browse" })
-  },
+  { },
   listensTo: ['playasset']
 },
 /** @Prototype */
 {
 	init : function(){
 		this.element.html(this.view("init.ejs"));
-    var technique = Grapplenode.Models.Technique;
-
-    /* this.element.find('#sublinks').suna_assetfilter({ model: technique, params: this.options.params,
-     filters: [{value: 'browse', label: 'Browse'},
-     {value: 'popular', label: 'Most Popular'},
-     {value: 'recent', label: 'Recently Added'},
-     {value: 'favorites', label: 'Favorites'},
-     {value: 'spotlight', label: 'Spotlight'}
-     ]});*/
-
 	},
 
-  "{$.route} tab set": function (clientState, ev, val) {
-    }
+  "{$.route} nav set": function (clientState, ev, val) {
+    if (val == 'techniques')
+      this.setfeatured();
+  },
+
+  "{$.route} techniques set": function (clientState, ev, val) {
+    this.element.find('.page-menu .active').removeClass('active').end()
+      .find('.page-menu li a[alt ='+val+']').parent().addClass('active')
+
+      if (val == 'featured')
+        this.setfeatured();
+      else if (val == 'sequences')
+        this.setsequences();
+      else if (val == 'popular')
+        this.setpopular();
+  },
+
+  ".page-menu li a click": function(elem, ev)
+  {
+    $.route.attrs({techniques:$(elem).attr('alt')}, true);
+  },
+
+  setfeatured: function()
+  {
+    var technique = Grapplenode.Models.Technique;
+    this.element.find('.takedowns').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.passes').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.sweeps').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.tc').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.escapes').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.submissions').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+  },
+
+  setsequences: function()
+  {
+    var technique = Grapplenode.Models.Technique;
+    this.element.find('.takedowns').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.passes').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+/*
+    this.element.find('.sweeps').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.tc').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+*/
+    this.element.find('.escapes').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.submissions').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+  },
+
+  setpopular: function()
+  {
+
+    var technique = Grapplenode.Models.Technique;
+     /*this.element.find('.takedowns').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.passes').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+*/
+    this.element.find('.sweeps').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.tc').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.escapes').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+
+    this.element.find('.submissions').suna_assetlist({ model: technique, params: this.options.params,
+      listtemplate: '//grapplenode/techniques/views/list_template.ejs'
+    });
+  }
+
+
 })
 
 });
